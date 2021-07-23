b2c8102ebd1ec27a584a02d53f248ad35a98b7e6
smy999 <33407191+smy999@users.noreply.github.com>
2021-07-23 15:30:29 +0900

    Update main.js

--- step-01/js/main.js
+++ step-01/js/main.js
@@ -52,11 +52,3 @@ function handleLocalMediaStreamError(error) {
 navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
   .then(gotLocalMediaStream).catch(handleLocalMediaStreamError);
 
-
-
-  // ** localStream은 전역 객체이므로 브라우저 콘솔에서 검사할 수 있다. **
-// localStream.getVideoTracks()은 무엇을 반환하는가?
-// localStream.getVideoTracks()[0].stop()의 결과는?
-// Look at the constraints object: what happens when you change it to {audio: true, video: true}? 접근 권한을 변경하면?
-// 임의로 설정한 비디오의 스트림 크기말고, 카메라의 원래 사이즈를 얻는 방법은?
-// css 설정을 통해 비디오와 svg 필터를 설정해
